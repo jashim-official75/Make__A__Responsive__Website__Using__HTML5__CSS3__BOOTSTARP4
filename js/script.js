9c9d81a0ad749bbafc4b8b6f9a19b71a5e3db4e8
jashimuddin360 <jashimuddinweb360@gmail.com>
2021-01-11 14:02:35 +0600

    this is first commmit in this project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+  // Collapse Navbar
+window.addEventListener('scroll',function(){
+
+  const header = document.querySelector('header');
+  header.classList.toggle("sticky",window.scrollY > 0);
+
+});
